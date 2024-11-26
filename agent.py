7903e8e3539110093452b401b0e8eec23c767a21
Othmane Mahfoud <mahfoud.othmane97@gmail.com>
2024-11-27 00:03:37 +0100

    switched to langgraph ReAct agent

--- agent.py
+++ agent.py
@@ -1,11 +1,24 @@
+import os
 import datetime
 from langchain.agents import initialize_agent, Tool
 from langchain_openai import ChatOpenAI
+from langchain.memory import ChatMessageHistory
+from langchain.schema import messages_to_dict, messages_from_dict
+from langchain_core.prompts import PromptTemplate
+
+from langgraph.prebuilt import create_react_agent
+
 from tools.weather import current_weather, weather_forecast
 from tools.navigation import transportation_directions
 from tools.events import event_tool
 from tools.landmarks import landmark_tool
 from tools.dining import dining_places_tool
+
+from dotenv import load_dotenv
+
+# Load environment variables
+load_dotenv(override=True)
+OPENAI_API_KEY = os.getenv('OPENAI_API_KEY')
 
 # Define the tools
 tools = [
@@ -17,13 +30,9 @@ tools = [
     Tool(name="Dining Places Tool", func=dining_places_tool, description="Retrieve information about Restaurants, Coffee shops, Bars and other types of dining institutions in Luxembourg.")
 ]
 
-# Initialize the chat model
-chat = ChatOpenAI(temperature=0.5)
-
 # Create prompt to direct the agent
-custom_prompt = f"""
-You are an assistant that answers user questions by using the most relevant tool. 
-You must know that today is {datetime.date.today()}.
+custom_prompt = """
+You are an assistant that answers the question: {question} by using the most relevant tool.
 If the user asks about current weather, ONLY use the Current Weather Tool and no other tool. 
 If the user asks about weather forecast, ONLY use the Weather Forecast Tool and no other tool. 
 If the user asks about events, activities, concerts etc., ONLY use the Event Tool and no other tool. 
@@ -34,10 +43,13 @@ If none of the tools are useful to answer the query, try to answer from your own
 Never mix information from multiple tools UNLESS explicitly asked by the human.
 """
 
-# Create the agent with the tools
-agent = initialize_agent(tools, chat, agent="zero-shot-react-description", verbose=True, prefix=custom_prompt)
+llm = ChatOpenAI(model="gpt-4o-mini", api_key=OPENAI_API_KEY, temperature=0.5)
 
-# Test the agent
-query = "What is the adress of coffee shop Oberweis?"
-response = agent.invoke(query)
-print("Agent Response:", response)
+agent = create_react_agent(llm, tools=[current_weather, weather_forecast, transportation_directions, event_tool, landmark_tool, dining_places_tool])
+
+history = ChatMessageHistory()
+history.add_ai_message("Hi, ask me anything about places, event, weather or transportation in Luxembourg")
+history.add_user_message("Will it rain in luxembourg tomorrow?")
+
+messages = agent.invoke(history)
+print(messages['messages'][-1].content)
