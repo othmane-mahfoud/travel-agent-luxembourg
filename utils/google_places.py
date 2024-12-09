b4af79ede0b730fb9e45a66dd4864cb33c1fb4bb
Othmane Mahfoud <mahfoud.othmane97@gmail.com>
2024-12-09 10:43:55 +0100

    fixed the google api to get places

--- utils/google_places.py
+++ utils/google_places.py
@@ -1,89 +1,113 @@
-import googlemaps
 import os
-import json
-import csv
-
+import requests
+import pandas as pd
 from dotenv import load_dotenv
 
-## Get places function, returns a list of places
 load_dotenv(override=True)
 
-gmaps = googlemaps.Client(key=os.getenv("GOOGLE_API_KEY"))
+API_KEY = os.getenv("GOOGLE_API_KEY")
+location = '49.6112809,6.1236146'
+radius = 20000
+place_type = 'restaurant'
+keyword = 'restaurant'
 
-def get_places(city, place_type="restaurant"):
-    ## Get coordinates of city
-    geocode_result = gmaps.geocode(city)
-    if geocode_result:
-        ## Get coordinates of the city
-        lat = geocode_result[0]['geometry']['location']['lat']
-        lng = geocode_result[0]['geometry']['location']['lng']
+# Step 1: Perform Nearby Search
+def get_nearby_places():
+    nearby_search_url = (
+        f'https://maps.googleapis.com/maps/api/place/nearbysearch/json'
+        f'?location={location}&radius={radius}&type={place_type}&keyword={keyword}&key={API_KEY}'
+    )
+    all_results = []
+    while nearby_search_url:
+        response = requests.get(nearby_search_url)
+        data = response.json()
+        results = data.get('results', [])
+        all_results.extend(results)
         
-        ## restaurants nearby
-        places_results = gmaps.places_nearby((lat, lng), radius=10000, type=place_type)        
-        
-        ## Get the names
-        places = []
-        service_fields = [
-            'serves_beer', 'serves_breakfast', 'serves_brunch', 
-            'serves_dinner', 'serves_lunch', 'serves_vegetarian_food', 
-            'serves_wine', 'takeout'
-        ]
-        
-        for place in places_results.get('results',[]):
-            name = place.get('name', 'N/A')
-            rating = place.get('rating', 'N/A')
-            price_level = place.get('price_level', 'N/A')
-            #location = place['geometry']['location']
-            description = place.get('vicinity', 'No description available')
-            place_id = place.get('place_id', None)
-            types = place.get('types',None)
-            n_ratings = place.get("user_ratings_total", 'N/A')
-            types = types[0]
+        # Check if there is a next page
+        next_page_token = data.get('next_page_token')
+        if next_page_token:
+            import time
+            time.sleep(2)  # Required delay before using the next_page_token
+            nearby_search_url = (
+                f'https://maps.googleapis.com/maps/api/place/nearbysearch/json'
+                f'?pagetoken={next_page_token}&key={API_KEY}'
+            )
+        else:
+            nearby_search_url = None  # No more pages
             
-            ## get website and opeining hours 
-            website = None
-            opening_hours = None
-            phone_number = None
-            wheelchair = None
-            services = {field: "N/A" for field in service_fields}
-            
-            if place_id:
-                place_details = gmaps.place(place_id=place_id)
-                website = place_details.get('result', {}).get('website', None)
-                # Extract opening hours if available
-                opening_hours = place_details.get('result', {}).get('opening_hours', {}).get('weekday_text', None)
-                phone_number = place_details.get('result', {}).get('international_phone_number', "N/A")
-                ## Features of the place
-                wheelchair = place_details.get('result', {}).get('wheelchair_accessible_entrance', 'N/A')
-                result_details = place_details.get('result', {})
-                for field in service_fields:
-                    services[field] = result_details.get(field, "N/A")
-                
-            places.append({
-                'FSQ_ID': None,
-                'Name': name,
-                'Category': types,
-                'Icon': None,
-                "Latitude": None,
-                "Longitude": None,
-                "Region": None,
-                "Formatted Address": description,
-                "Description": f"Opening Hours {opening_hours} - Accessibility {wheelchair}",
-                "Phone Number": phone_number,
-                'Rating': rating,
-                'Price': price_level,
-                "Tips": None
-            })
-            
-        ## Save the places into csv
-        with open("data/google_places.csv", "w", newline='', encoding="utf-8") as csv_file:
-            writer = csv.DictWriter(csv_file, fieldnames=places[0].keys())
-            writer.writeheader()
-            writer.writerows(places)
-    
-        return places
-    else:
-        print("place not found")
+    return all_results
+
+# Step 2: Get Place Details
+def get_place_details(place_id):
+    fields = 'name,formatted_address,geometry,photos,types,formatted_phone_number,rating,price_level,reviews'
+    place_details_url = (
+        f'https://maps.googleapis.com/maps/api/place/details/json'
+        f'?place_id={place_id}&fields={fields}&key={API_KEY}'
+    )
+    response = requests.get(place_details_url)
+    return response.json().get('result', {})
+
+# Step 3: Extract Desired Information
+def extract_image_url(photos):
+    if photos:
+        photo_reference = photos[0]['photo_reference']
+        return (
+            f'https://maps.googleapis.com/maps/api/place/photo'
+            f'?maxwidth=400&photoreference={photo_reference}&key={API_KEY}'
+        )
+    return 'None'
+
+def extract_reviews(reviews):
+    if not reviews:
+        return 'None'
+    review_texts = [review.get('text', 'No review text') for review in reviews[:3]]
+    return ' | '.join(review_texts)
+
+# Main Function
+def fetch_places_data():
+    places = get_nearby_places()
+    all_places_data = []
+
+    for place in places:
+        place_id = place['place_id']
+        details = get_place_details(place_id)
         
-if __name__ == "__main__":
-    get_places(city="Luxembourg")
+        # Extract details
+        name = details.get('name', 'None')
+        address = details.get('formatted_address', 'None')
+        geometry = details.get('geometry', {}).get('location', {})
+        latitude = geometry.get('lat', 'None')
+        longitude = geometry.get('lng', 'None')
+        types = details.get('types', [])
+        phone_number = details.get('formatted_phone_number', 'None')
+        rating = details.get('rating', 'None')
+        price_level = details.get('price_level', 'None')
+        reviews = extract_reviews(details.get('reviews', []))
+        image_url = extract_image_url(details.get('photos', []))
+
+        # Append data
+        all_places_data.append({
+            'Name': name,
+            'Category': ', '.join(types),
+            'Image URL': image_url,
+            'Latitude': latitude,
+            'Longitude': longitude,
+            'Formatted Address': address,
+            'Description': None,
+            'Phone Number': phone_number,
+            'Rating': rating,
+            'Price Level': 2 if 'None' else price_level,
+            'Reviews': reviews
+        })
+        
+        all_places_df = pd.DataFrame(all_places_data)
+
+    return all_places_df
+
+# Run the script and print results
+if __name__ == '__main__':
+    places_df = fetch_places_data()
+    places_df.to_csv("data/google_places.csv", index=False)
+    print(places_df.shape)
+    print("Google Places Successfully Saved")
